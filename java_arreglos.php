cf7b46661ffc12c21c81cef1ed0a2688a837bc8f
pedro <sanchezsandovalpedroantonio@gmail.com>
2025-11-09 13:30:45 -0700

    los diseños para java y c++

---
+++ java_arreglos.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<style>
+		body{
+			background: lavender;
+		}
+
+		button{
+			padding: 12px 30px;
+            font-size: 16px;
+            font-weight: 500;
+            color: white;
+            background: cadetblue;
+            border: 2px;
+            border-radius: 6px;
+            cursor: pointer;
+            transition: all 0.2s ease;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            
+
+		}
+
+		header{
+			text-align: center;
+			font-size: 50px;
+			padding: 40px;
+			background: cadetblue;
+			color: white;
+
+
+		}
+
+		.language-logo {
+            position: absolute;
+            top: 20px;
+            right: 30px;
+            width: 60px;
+            height: 60px;
+            object-fit: contain;
+            opacity: 0.9;
+        }
+
+
+	</style>
+</head>
+<body>
+
+<a href="index.php">
+  <button>Regresar</button>
+</a>
+<br><br>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="Logo Java" class="language-logo">
+<br><br>
+<header>
+	Ejercicios con arreglos resueltos Java
+</header>
+</body>
+</html>
